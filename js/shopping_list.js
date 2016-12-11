dee1f9426abb56b608b8ce06b889fb85c550a498
caitypizza <caitykondo@gmail.com>
2016-12-10 21:15:11 -1000

    created ShoppingListClass, methods need to be tested more

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -1,10 +1,28 @@
-// class ShoppingList{
-//   constructor(items){
-//     this.items = [];
-
-//   }
-//   addItem(item){
-//     if(item === this.)
-
-//   }
-// }
+class ShoppingList{
+  constructor(){
+    this.items = [];
+  }
+  addItem(item){
+    if(item instanceof ShoppingListItem){
+      this.items.push(item);
+    }else{
+      throw Error(`${item} is not an item.`);
+    }
+  }
+  removeItem(ShoppingListItem){
+    if(this.items.indexOf(ShoppingListItem) !== -1){
+      this.items.splice(indexOf(ShoppingListItem), 1);
+    }if(arguments.length === 0){
+      this.items.pop();
+    }else{
+      throw Error(`${ShoppingListItem} is not an item.`);
+    }
+  }
+  render(){
+    var shoppingListHTML = [];
+    for(var i = 0; i < this.items.length - 1; i++ ){
+      shoppingListHTML.push(this.items[i]);
+    }
+    return `<ul>${shoppingListHTML}</ul>`;
+  }
+}
