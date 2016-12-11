b440b98a15a9325b3900ae3c9dc77735ccc8060b
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-12-10 18:55:04 -1000

    new files

--- js/shopping_list_item_test.js
+++ js/shopping_list_item_test.js
@@ -1,1 +1,10 @@
-console.log('sanity check');
+const should = chai.should();
+const expect = chai.expect();
+
+describe('shoppingListItem', () => {
+  let listItem = new ShoppingListItem();
+  it('should be a class', ()=>{
+    //should.be.a.function
+    ShoppingListItem.should.be.a.function;
+  });
+});
