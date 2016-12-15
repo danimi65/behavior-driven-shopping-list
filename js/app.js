2179c7096061621e54b04fe9815e676b01fc4474
caitypizza <caitykondo@gmail.com>
2016-12-14 19:36:36 -1000

    moved creating DOM elements from app.js to being  hardcoded in index.html

--- js/app.js
+++ js/app.js
@@ -1,18 +1,9 @@
 
 let content = document.getElementById('content');
 let myShoppingList = new ShoppingList;
+const nameInput = document.getElementById('itemName');
+const descriptionInput = document.getElementById('itemDescription');
 
-let nameInput = document.createElement('input');
-nameInput.className ='nameInput';
-// nameInput.setAttribute('type', 'text');
-nameInput.setAttribute('placeholder', 'Name');
-content.appendChild(nameInput);
-
-let descriptionInput = document.createElement('input');
-descriptionInput.className ='descriptionInput';
-// descriptionInput.setAttribute('type', 'text');
-descriptionInput.setAttribute('placeholder', 'Description');
-content.appendChild(descriptionInput);
 
 function addToShoppingList(){
   let newItem = new ShoppingListItem(nameInput.value, descriptionInput.value);
@@ -22,9 +13,7 @@ function addToShoppingList(){
   shoppingListContainer.appendChild(myShoppingList.render());
 }
 
-let addListItemButton = document.createElement('button');
-content.appendChild(addListItemButton);
-addListItemButton.innerHTML = 'Add To Shopping List';
+let addListItemButton = document.getElementById('add_shopping_list_button');
 addListItemButton.addEventListener('click', addToShoppingList);
 
 function changeCheckStatus(idx, checkbox){
